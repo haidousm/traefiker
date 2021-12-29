374b24c5d0a11783c55d56ff273d1e346611ba4f
haidousm <haidous.m@gmail.com>
2021-12-29 10:35:33 +0200

    refactor /api/services handler

--- src/pages/api/services/index.ts
+++ src/pages/api/services/index.ts
@@ -4,21 +4,28 @@ import * as docker from "../../../lib/docker";
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
     const { method } = req;
     if (method === "GET") {
-        return res.status(200).json(docker.getAllServices());
+        handleGetRequest(req, res);
     } else if (method === "POST") {
-        const { name, image, hosts, order } = req.body;
-        const _service = docker.createService(name, image, hosts, order);
-        docker.saveService(name, _service);
-        if (process.env.NODE_ENV === "production") {
-            docker.launchDockerCompose(() => {
-                res.status(200).json(_service);
-            });
-        } else {
-            setTimeout(() => {
-                res.status(200).json(_service);
-            }, 5000);
-        }
+        handlePostRequest(req, res);
     }
 };
 
+const handleGetRequest = (req: NextApiRequest, res: NextApiResponse) => {
+    res.status(200).json(docker.getAllServices());
+};
+
+const handlePostRequest = (req: NextApiRequest, res: NextApiResponse) => {
+    const { name, image, hosts, order } = req.body;
+    const _service = docker.createService(name, image, hosts, order);
+    docker.saveService(name, _service);
+    if (process.env.NODE_ENV === "production") {
+        docker.launchDockerCompose(() => {
+            res.status(200).json(_service);
+        });
+    } else {
+        setTimeout(() => {
+            res.status(200).json(_service);
+        }, 5000);
+    }
+};
 export default handler;

5884d9cf020faef0db3296b8b2affed0a7642bc3
haidousm <haidous.m@gmail.com>
2022-02-04 19:17:49 +0200

    add route to create service

--- api/src/routes/services.ts
+++ api/src/routes/services.ts
@@ -1,10 +1,16 @@
 import express from "express";
-import { Service } from "../models/Service";
+import { Service, ServiceModel } from "../models/Service";
 const router = express.Router();
 
 router.get("/", async (_req, res) => {
     const services = await Service.find();
     res.json(services);
+});
+
+router.post("/", async (req, res) => {
+    const serviceRequest: ServiceModel = req.body;
+    const service = await Service.create(serviceRequest);
+    res.json(service);
 });
 
 export default router;

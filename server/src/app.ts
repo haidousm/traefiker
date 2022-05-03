10f13869000796f5ad0f510fee3adcda33c6b3a0
haidousm <haidous.m@gmail.com>
2022-05-03 18:37:06 +0300

    switch to using config files vs dotenv files

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,6 @@
-console.log("hello!");
+import express from "express";
+import config from "config";
+
+const app = express();
+const port = config.get<number>("PORT") || 8010;
+app.listen(port, () => console.log(`Express server started on port ${port}`));

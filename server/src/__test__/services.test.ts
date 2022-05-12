5f222dc8ada0a3b535dafd8c02a2308b1f3ac174
haidousm <haidous.m@gmail.com>
2022-05-12 11:55:01 +0300

    create tests for the createServiceRoute

--- server/src/__test__/services.test.ts
+++ server/src/__test__/services.test.ts
@@ -3,8 +3,39 @@ import supertest from "supertest";
 import createServer from "../utils/server";
 
 import * as ServicesService from "../services/services.service";
+import * as DockerLib from "../../libs/docker";
 import passport from "passport";
 import { NextFunction, Request, Response } from "express";
+import { CreateServiceRequest } from "../schemas/services.schema";
+
+const createServiceRequest: CreateServiceRequest = {
+    name: "httpd",
+    image: "httpd",
+    hosts: ["httpd.haidousm.com"],
+};
+
+const createdService = {
+    name: "httpd",
+    status: "pulling",
+    image: {
+        name: "httpd",
+        tag: "latest",
+        repository: "haidousm/httpd",
+        identifier: "haidousm/httpd:latest",
+        createdAt: new Date().toString(),
+    },
+    hosts: ["httpd.haidousm.com"],
+    redirects: [],
+    environments: [],
+    order: 0,
+    tag: "traefiker_httpd",
+    containerId: "",
+    createdAt: new Date().toString(),
+};
+
+const createdContainer = {
+    id: "12345",
+};
 
 const app = createServer();
 
@@ -18,7 +49,7 @@ describe("services", () => {
         });
         describe("when user is logged in", () => {
             it("should return all services", async () => {
-                passport.authenticate = jest.fn(() => {
+                jest.spyOn(passport, "authenticate").mockImplementation(() => {
                     return (
                         req: Request,
                         res: Response,
@@ -35,6 +66,51 @@ describe("services", () => {
                 const response = await supertest(app).get("/services");
                 expect(response.status).toBe(200);
                 expect(response.body).toEqual([]);
+            });
+        });
+    });
+    describe("create new service", () => {
+        describe("when user is not logged in", () => {
+            it("should return 401", async () => {
+                const response = await supertest(app)
+                    .post("/services/create")
+                    .send(createServiceRequest);
+                expect(response.status).toBe(401);
+            });
+        });
+        describe("when user is logged in", () => {
+            it("should create service", async () => {
+                passport.authenticate = jest.fn(() => {
+                    return (
+                        req: Request,
+                        res: Response,
+                        next: NextFunction
+                    ) => {
+                        next();
+                    };
+                });
+
+                jest.spyOn(ServicesService, "createService")
+                    // @ts-ignore
+                    .mockReturnValueOnce(createdService);
+
+                jest.spyOn(DockerLib, "createContainer")
+                    //@ts-ignore
+                    .mockReturnValueOnce(createdContainer);
+
+                jest.spyOn(ServicesService, "attachContainerToService")
+                    //@ts-ignore
+                    .mockImplementationOnce(() => {
+                        createdService.containerId = createdContainer.id;
+                        createdService.status = "created";
+                        return createdService;
+                    });
+
+                const response = await supertest(app)
+                    .post("/services/create")
+                    .send(createServiceRequest);
+                expect(response.status).toBe(200);
+                expect(response.body).toEqual(createdService);
             });
         });
     });
